799fbb1745cec0031fe60665a4fc44dded5a9dcd
EpicApex <omrifridenberg@gmail.com>
2017-07-21 22:44:27 +0300

    21/7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,27 +10,99 @@ export class Hero {
   
 }
 
+const HEROES: Hero[] = [
+  { id: 1, name: 'Genji',       role: 'Offense ', nemesis: 'Mei / Symettra / McCree / Winston '},
+  { id: 2, name: 'McCree',      role: 'Offense ', nemesis: 'Mostly balanced (#Care from D.V.A / Winston / RoadHoag / Zarya'},
+  { id: 3, name: 'Phara',       role: 'Offense ', nemesis: 'Soldier / McCree / D.V.A'},
+  { id: 4, name: 'Soldier 76',  role: 'Offense' , nemesis: 'Mostly balanced (#Care from McCree / Symmetra / Sombra '},
+  { id: 5, name: 'Reaper',      role: 'Offense' , nemesis: 'D.V.A / McCree '},
+  { id: 6, name: 'Sombra',      role: 'Offense' , nemesis: 'D.V.A / McCree / Soldier 76 / Winston '},
+  { id: 7, name: 'Tracer',      role: 'Offense' , nemesis: 'D.V.A / McCree / RoadHoag / Soldier'},
+  { id: 8, name: 'DoomFist',    role: 'Offense' , nemesis: ' N/A '}
+  
+];
+
+
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   template: `
-              <h1> {{title}} </h1> \n
-              <h2> Hero Description SUP                                   </h2>  \n 
-              <div><label> ID </label> {{hero.id}}                        </div> \n
-              <div>
-                    <label> Name   - </label> 
-                    <input [(ngModel)]="hero.name" placeholder="name">
-              </div>                                                             \n
-              <div><label> Role - </label> {{hero.role}}                  </div> \n
-              <div> {{hero.nemesis}} is its nemesis                       </div>
-            `
-  
-})
+              <h1> {{title}} </h1>           \n
+              <h2> Magical creatures  </h2>  \n
+              <ul class="heroes">
+                <li *ngFor="let hero of heroes">
+                  <a><span class="badge">{{hero.id}}</span> - {{hero.name}} - {{hero.role}} - {{hero.nemesis}}</a>
+                </li>
+              </ul>
+
+            `,
+  styles:     [`
+                  .selected {
+                    background-color: #CFD8DC !important;
+                    color: white;
+                  }
+                  .heroes {
+                    margin: 0 0 2em 0;
+                    list-style-type: none;
+                    padding: 0;
+                    width: 30em;
+                  }
+                  .heroes li {
+                    cursor: pointer;
+                    position: relative;
+                    left: 0;
+                    background-color: #EEE;
+                    margin: .5em;
+                    padding: .3em 0;
+                    height: 3.2em;
+                    border-radius: 4px;
+                  }
+                  .heroes li.selected:hover {
+                    background-color: #BBD8DC !important;
+                    color: white;
+                  }
+                  .heroes li:hover {
+                    color: #607D8B;
+                    background-color: #DDD;
+                    left: .1em;
+                  }
+                  .heroes .text {
+                    
+                    position: relative;
+                    top: -3px;
+                  }
+                  .heroes .badge {
+                    display: inline-block;
+                    font-size: small;
+                    color: white;
+                    padding: 0.8em 0.7em 0 0.7em;
+                    background-color: #607D8B;
+                    line-height: 1em;
+                    position: relative;
+                    left: -1px;
+                    top: -4px;
+                    height: 3.8em;
+                    margin-right: 1.6em;
+                    border-radius: 100px 0 0 100px;
+                  }
+
+                  a {
+                      display: flex;
+                      align-items: center;
+                      justify-content: center; 
+                      height: 100%; 
+                     }
+
+                `]
+                  
+              })
+
+
+
 
 export class AppComponent {
-  
-  title  = 'Tour Of Heroes';
+  title  = 'Activision - Blizzard - Overwatch playground';
+  heroes = HEROES;
   hero: Hero = {
     id:       1,
     name:     'Winston',
@@ -39,6 +111,8 @@ export class AppComponent {
   };
 
 }
+
+
 
 
 
