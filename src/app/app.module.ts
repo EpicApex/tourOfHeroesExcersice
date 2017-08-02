80f75d7fd34d771dfa0e387407a8e5c8ed70cea1
EpicApex <omrifreidenberg@gmail.com>
2017-08-02 09:50:43 +0300

    Add files via upload
    2/8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,41 @@ import { FormsModule  }         from '@angular/forms';
 
 //AppComponent related components
 import { AppComponent }         from './app.component';
+import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
+import { DashboardComponent}    from './dashboard.component';
+
+//Router related components
+import { RouterModule }         from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
-    //NgModule -here- => Not a module error
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    
+    RouterModule.forRoot ([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+    ])
   ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
